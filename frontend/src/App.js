e8e5694fbfea7f6a26ab36fa88068e8ca8946da5
Pramukh2002 <36731590+Pramukh2002@users.noreply.github.com>
2022-03-04 22:12:04 +0530

    First Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import { ContactUs } from './components/ContactUs';
+
+function App() {
+  return (
+      <ContactUs></ContactUs>
+    );
+}
+
+export default App;
